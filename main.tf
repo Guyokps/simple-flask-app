696d7d9561c6cd41919da270100e2bdb1103c743
Guyokps <124910053+Guyokps@users.noreply.github.com>
2023-07-07 14:58:24 +0300

    Commented out security_group_ids for a test

--- main.tf
+++ main.tf
@@ -9,14 +9,25 @@ variable "create_instance" {
   default = false  # Set this to false if you don't want to create the instance
 }
 
+data "aws_security_group" "existing_ssh_security_group" {
+  name = "ssh-security-group"
+}
+
 resource "aws_instance" "example" {
   count         = var.create_instance ? 1 : 0  # Create the instance only if var.create_instance is true
   ami           = "ami-053b0d53c279acc90"  # Replace with the desired AMI ID
   instance_type = "t2.micro"
   key_name      = "terraform"
-  #security_group_ids = ["sg-0cf32d8b7883edccf"]
   tags = {
     Name = "TF Instance"
+  }
+
+  # Check if the security group already exists
+  dynamic "security_group_ids" {
+    for_each = data.aws_security_group.existing_ssh_security_group.id != null ? [data.aws_security_group.existing_ssh_security_group.id] : []
+    content {
+      value = security_group_ids.value
+    }
   }
 
   provisioner "remote-exec" {
@@ -38,10 +49,11 @@ resource "aws_instance" "example" {
   }
 }
 
-# Create a security group to allow SSH connections
+# Create a security group only if it doesn't already exist
 resource "aws_security_group" "ssh_security_group" {
-  name        = "ssh-security-group"
-  description = "Allow SSH connections"
+  count        = data.aws_security_group.existing_ssh_security_group.id == null ? 1 : 0
+  name         = "ssh-security-group"
+  description  = "Allow SSH connections"
 
   ingress {
     from_port   = 22
