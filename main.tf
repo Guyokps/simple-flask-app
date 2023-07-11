5dcabb63bf037339194e3125239c2e89f0136c0e
Guyokps <124910053+Guyokps@users.noreply.github.com>
2023-07-11 13:23:17 +0300

    Test

--- main.tf
+++ main.tf
@@ -9,24 +9,24 @@ variable "create_instance" {
   default = true  # Set this to false if you don't want to create the instance
 }
 
-resource "aws_security_group" "ssh" {
-  name        = "ssh-security-group"
-  description = "Allow SSH access"
+#resource "aws_security_group" "ssh" {
+#  name        = "ssh-security-group"
+#  description = "Allow SSH access"
+#
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
+#Create Instance Free Tier
 resource "aws_instance" "example" {
   count         = var.create_instance ? 1 : 0
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = "terraform"
-
   tags = {
     Name = "FlaskApp"
   }
